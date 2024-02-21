bbbef3a3bcdeb10924e90d2558a17d392cad1aa5
Archin0 <paboene25@gmail.com>
2024-02-21 12:33:04 +0700

    Percobaan 1

---
+++ Jobsheet2/Buku08.java
@@ -0,0 +1,39 @@
+package Jobsheet2;
+
+/**
+ * Buku08
+ */
+public class Buku08 {
+
+    String judul, pengarang;
+    int halaman, stok, harga;
+
+    void tampilInformasi() {
+        System.out.println("Judul: " + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Jumlah halaman: " + halaman);
+        System.out.println("Sisa stok: " + stok);
+        System.out.println("Harga: Rp" + harga);
+    }
+
+    void terjual(int jml) {
+        if(stok > 0) {
+            stok -= jml;
+        }
+        else{
+            stok = 0;
+        }
+    }
+
+    void restock(int jml) {
+        stok += jml;
+    }
+
+    void gantiHarga(int hrg) {
+        harga = hrg;
+    }
+
+    public static void main(String[] args) {
+        Buku b1 = new Buku();
+    }
+}
